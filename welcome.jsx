d513cbc5c11fae4c630dfdcdc0daceed1777187f
Dikshasonawane <92422873+Dikshasonawane@users.noreply.github.com>
2022-02-03 12:33:49 +0530

    Add files via upload

---
+++ welcome.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+function Welcome({name}) {
+    return (
+    <>
+    
+    <h1>Hey! {name}!</h1>
+    <h2>Welcome to Newton School.</h2>
+
+    </>)
+  }
+
+  export default Welcome;
